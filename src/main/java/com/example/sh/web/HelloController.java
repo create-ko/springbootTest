09a3f9e5528b32853393f7a786f42a66b1c280d8
create-ko <create_ko@naver.com>
2021-06-04 17:54:50 +0900

    Chapter 2

--- src/main/java/com/example/sh/web/HelloController.java
+++ src/main/java/com/example/sh/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.sh.web;
 
+import com.example.sh.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +13,10 @@ public class HelloController {
         return "hello";
     }
 
-//    @GetMapping("/hello/dto")
-//    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
-//        return new HelloResponseDto(name, amount);
-//    }
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
 
 }
 
